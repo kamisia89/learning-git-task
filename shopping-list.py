bd066c004e4aab5c6b5b4d5cdc9b8c60de2b9cd2
kamisia89 <131877928+kamisia89@users.noreply.github.com>
2023-04-27 10:36:54 +0100

    full code added

--- shopping-list.py
+++ shopping-list.py
@@ -1,1 +1,11 @@
-shopping-list.py
+shopping = {
+    'piekarnia': ['Chleb', 'Pączek', 'Bułki'],
+    'warzywniak': ['Marchew', 'Seler', 'Rukola']
+}
+print(
+    f"Idę do piekarnia, kupuję tu następujące rzeczy: {shopping['piekarnia']}")
+print(
+    f"Idę do warzywniak, kupuję tu następujące rzeczy: {shopping['warzywniak']}")
+print(
+    f"W sumie kupuję: {len(shopping['piekarnia']+shopping['warzywniak'])} produktów")
+
