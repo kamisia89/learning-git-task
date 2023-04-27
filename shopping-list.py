d02a823a106c43e5ab06a2f1944ee2b7644a44c8
kamisia89 <131877928+kamisia89@users.noreply.github.com>
2023-04-27 10:58:15 +0100

    simmilar second list

--- shopping-list.py
+++ shopping-list.py
@@ -9,4 +9,13 @@ print(
 print(
     f"W sumie kupuję: {len(shopping['piekarnia']+shopping['warzywniak'])} produktów")
 print("zdublowana lista")
-
+shopping = {
+    'piekarnia': ['Chleb', 'Pączek', 'Bułki'],
+    'warzywniak': ['Marchew', 'Seler', 'Rukola']
+}
+print(
+    f"Idę do piekarnia, kupuję tu następujące rzeczy: {shopping['piekarnia']}")
+print(
+    f"Idę do warzywniak, kupuję tu następujące rzeczy: {shopping['warzywniak']}")
+print(
+    f"W sumie kupuję: {len(shopping['piekarnia']+shopping['warzywniak'])} produktów")
